b33ddcd60b4340d377ccaa8a620638dd20ce1fff
skylanche <remoneteleporta@gmail.com>
2026-08-18 14:52:29 +0530

    frontend of app

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Header from '@/components/Header.vue'
+
+const header1Txt = ref("Gemini Vector Embedding Generator")
+const buttonTxt = ref("Get Embedding")
+</script>
+
+<template>
+<Header/>
+<h1>{{header1Txt}}</h1>
+
+  <textarea id="textInput" rows="3" placeholder="Enter text..."></textarea>
+  <button onclick="generateEmbedding()">{{buttonTxt}}</button>
+
+  <pre id="output"></pre>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import logo from '@/assets/logo.svg'
+</script>
+
+<template>
+<header>
+    <img :src="logo" alt="Logo" />
+</header>
+</template>
+
+<style scoped>
+header{
+    display: flex;
+    margin-bottom: 20px;
+    justify-content: center;
+}
+img{
+    width: 100px;
+}
+</style>
