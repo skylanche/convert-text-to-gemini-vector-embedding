3e432097072cb1ab3c4293b3cb4686b1f78c2398
skylanche <sakthivigneshvk@gmail.com>
2026-09-01 01:09:19 +0530

    css modifications, Button Disable function CSS, Status Message display function

--- text-to-gemini-vector-backend/src/services/gemini.ts
+++ text-to-gemini-vector-backend/src/services/gemini.ts
@@ -4,7 +4,7 @@ export default async function createEmbedding(
     text: string
 ): Promise<number[]> {
 
-    const apiKey = process.env.GEMINI_API_KEY;
+    const apiKey: string | undefined = process.env.GEMINI_API_KEY;
 
     if (!apiKey) {
         throw new Error("GEMINI_API_KEY is not configured.");
